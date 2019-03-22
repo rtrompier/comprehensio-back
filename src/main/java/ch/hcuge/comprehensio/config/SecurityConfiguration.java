68f6249ef6a2185f4d89ae18843dc1e07c6ca55d
HUGE\gmer <guerric.merle@hcuge.ch>
2019-03-22 13:37:03 +0100

    security config with role mapping

--- src/main/java/ch/hcuge/comprehensio/config/SecurityConfiguration.java
+++ src/main/java/ch/hcuge/comprehensio/config/SecurityConfiguration.java
@@ -1,16 +1,34 @@
 package ch.hcuge.comprehensio.config;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+	
+	private static final String KEYCLOAK_REALM_ACCESS = "realm_access";
+	private static final String KEYCLOAK_ROLES = "roles";
+
 
 	@Bean
 	SecurityWebFilterChain configure(ServerHttpSecurity http) {
@@ -22,8 +40,31 @@ public class SecurityConfiguration {
 	    	.anyExchange().permitAll()
 	    	.and()
 			.oauth2ResourceServer()
-			.jwt();
+			.jwt()
+			.jwtAuthenticationConverter(grantedAuthoritiesExtractor())
+			
+			;
 	 // @formatter:on
 		return http.build();
 	}
+	
+	Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+	    GrantedAuthoritiesExtractor extractor = new GrantedAuthoritiesExtractor();
+	    return new ReactiveJwtAuthenticationConverterAdapter(extractor);
+	}
+	
+	static class GrantedAuthoritiesExtractor extends JwtAuthenticationConverter {
+	    protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+	    	// @formatter:off
+	    				JSONObject realmAccess = (JSONObject) jwt.getClaims().get(KEYCLOAK_REALM_ACCESS);
+	    				JSONArray roles = (JSONArray) realmAccess.get(KEYCLOAK_ROLES);
+	    				return roles.stream()
+	    						.map(Object::toString)
+	    						.map(SimpleGrantedAuthority::new)
+	    						.collect(Collectors.toSet());
+	    				// @formatter:on
+	    }
+	}
+
+	
 }
