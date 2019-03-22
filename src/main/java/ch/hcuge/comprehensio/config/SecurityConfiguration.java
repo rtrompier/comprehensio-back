ccf714a9d39954e0412cc893c8ca688f8d20c0d8
HUGE\gmer <guerric.merle@hcuge.ch>
2019-03-22 11:01:24 +0100

    security config

---
+++ src/main/java/ch/hcuge/comprehensio/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package ch.hcuge.comprehensio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration {
+
+	@Bean
+	SecurityWebFilterChain configure(ServerHttpSecurity http) {
+		// @formatter:off
+	    http
+	    .httpBasic().disable()
+	    .formLogin().disable()
+	    	.authorizeExchange()
+	    	.anyExchange().permitAll()
+	    	.and()
+			.oauth2ResourceServer()
+			.jwt();
+	 // @formatter:on
+		return http.build();
+	}
+}
