763d326c8512bb2e7359d3f7d1c42980067cb112
rtrompier <remy.trompier@gmail.com>
2019-03-22 09:42:05 +0100

    first commit

---
+++ src/main/java/ch/hcuge/comprehensio/ComprehensioApplication.java
@@ -0,0 +1,13 @@
+package ch.hcuge.comprehensio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprehensioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComprehensioApplication.class, args);
+    }
+
+}
