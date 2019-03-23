6f797e8287ac06597cd88572d4b4c74843d8adb7
HUGE\gmer <guerric.merle@hcuge.ch>
2019-03-23 04:41:36 +0100

    import

--- src/main/java/ch/hcuge/comprehensio/repository/TransactionRepository.java
+++ src/main/java/ch/hcuge/comprehensio/repository/TransactionRepository.java
@@ -1,9 +1,9 @@
 package ch.hcuge.comprehensio.repository;
 
-import ch.hcuge.comprehensio.entity.Transaction;
-import ch.hcuge.comprehensio.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import ch.hcuge.comprehensio.entity.Transaction;
 
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, String> {
