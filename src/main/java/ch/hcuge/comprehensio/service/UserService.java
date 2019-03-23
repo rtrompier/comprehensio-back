cf66d610f6cfb81e3d493eb4cb650ee35275de81
HUGE\gmer <guerric.merle@hcuge.ch>
2019-03-23 04:40:13 +0100

    import

--- src/main/java/ch/hcuge/comprehensio/service/UserService.java
+++ src/main/java/ch/hcuge/comprehensio/service/UserService.java
@@ -1,12 +1,12 @@
 package ch.hcuge.comprehensio.service;
 
-import ch.hcuge.comprehensio.entity.Lang;
-import ch.hcuge.comprehensio.entity.User;
-import ch.hcuge.comprehensio.repository.RoleRepository;
-import ch.hcuge.comprehensio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import ch.hcuge.comprehensio.entity.Lang;
+import ch.hcuge.comprehensio.entity.User;
+import ch.hcuge.comprehensio.repository.UserRepository;
 
 @Service
 public class UserService {
